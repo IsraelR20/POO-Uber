ad86a4686160f3dcf6146127f4fdb85fac0ed5b4
IsraelR20 <israelrojasvilca@gmail.com>
2022-07-08 12:38:06 -0500

    Creando objetos en Java

--- Java/Car.java
+++ Java/Car.java
@@ -3,4 +3,10 @@ public class Car {
     String license;
     String driver;
     Integer passenger;
+
+    void printDataCar() { // method to print the data of the car object
+        System.out.println("Car license: " + license);
+        System.out.println("Car driver: " + driver);
+        System.out.println("Car passenger: " + passenger);
+    }
 }
--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,16 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Car car = new Car(); // create a new car object
+        car.license = "ABC-123"; // assign a license to the car object
+        car.driver="Jhon el conductor"; // assign a driver to the car object
+        car.passenger = 4; // assign 3 passengers to the car object
+        car.printDataCar(); // print the data of the car object
+
+        Car car2 = new Car(); // create a new car object
+        car2.license = "XYZ-456"; // assign a license to the car object
+        car2.driver="Luis el conductor"; // assign a driver to the car object
+        car2.passenger = 2; // assign 2 passengers to the car object
+        car2.printDataCar(); // print the data of the car object
     }
 }
